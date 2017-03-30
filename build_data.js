25fd8827c841dfd2a3b1302b2d5d1d88cd2c4f6f
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-03-30 22:04:04 +0200

    Add build_data()

---
+++ build_data.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var val = require('tidyval');
+var path = require('tidypath');
+var objRef = require('objref');
+
+module.exports = function build_data(_db){
+	var _data = {};
+	
+	val(_db).validate('array');
+	
+	for (netPath in _db){	//file.netPath
+		_db[netPath]._data = _data;
+		
+		var treePath = path.tree(netPath);
+		var name = netPath.slice(treePath)
+
+		var dbTree = _data;
+		if (treePath.length){
+			dbTree = objRef(_data, treePath, path.delimiter, true);
+		}
+		
+		dbTree[name] = _db[netPath];
+	});
+	
+	return _db;
+};
