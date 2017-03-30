478f588e595c35a875756d4430b42fa586ade98a
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-03-30 22:03:44 +0200

    Add dbAddProp()

---
+++ dbAddProp.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var val = require('tidyval');
+var path = require('tidypath');
+var objRef = require('objref');
+
+module.exports = function addProp(_db, propName, propVal){
+	val(_db).validate('object');
+	val(propName).validate('string');
+	val(propName).invalidate('undefined');
+	
+	for (item in _db){
+		_db[item][propName] = propVal;
+	}
+	
+	return _db;
+};
