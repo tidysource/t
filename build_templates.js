0264a7681bb266aa3e668165e4752be912a4a6ae
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-03-30 22:03:19 +0200

    Add build_templates()

---
+++ build_templates.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var val = require('tidyval');
+var path = require('tidypath');
+var objRef = require('objref');
+
+module.exports = function build_templates(files){
+	_templates = {};
+	
+	files.map(function(template){
+		template.path = template.path.slice(config.folders.templates.length);
+		template.netPath = path.rmExt(template.path);
+		var treePath = path.tree(netPath);
+		var name = netPath.slice(treePath);
+		
+		var _templatesTree = templates;
+		if (treePath.length){
+			_templatesTree = objRef(_data, treePath, path.delimiter, true);
+		}
+		
+		_templatesTree[name] = template.content;
+	}
+	
+	return _templates;
+};
