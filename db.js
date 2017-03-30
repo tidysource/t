62994c4596b08cc5c4e0755502366f133180bdc2
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-03-30 22:02:12 +0200

    Add build_db()

---
+++ db.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var val = require('tidyval');
+var path = require('tidypath');
+var objRef = require('objref');
+
+module.exports = function build_db(files){
+	val(files).validate('array');
+	
+	var _db = {}
+	files.map(function(file){
+		val(file).validate('object');
+		val(file.netPath).validate('sting');
+		
+		if (typeof _db[file.netPath] === 'undefined'){
+			_db[file.netPath] = {};
+		}
+		
+		//Merge
+		for (prop in file.parsed){
+			if (typeof _db[file.netPath][prop] === 'undefined'){
+				_db[file.netPath][prop] = file.parsed[prop];
+			}
+			else{
+				throw new Error('Filename/filecontent conflict');
+			}
+		}
+	});
+
+	return _db;
+};
