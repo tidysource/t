f61989c8efbc57d7ecd345b490383709af927e12
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-04-14 00:26:20 +0200

    Tidy up

--- matchTemplates.js
+++ matchTemplates.js
@@ -7,20 +7,20 @@ var objRef = require('objref');
 module.exports = function matchTemplates(_db, templateFiles){
 	val(_db).validate('object');
 	val(templateFiles).validate('array');
-	
+
 	//Build index
 	var templates = {};
 	templateFiles.map(function(template){
 		templates[template.path] = template;
 	});
-	
+
 	//Match
 	for (var netPath in _db){
 		var item = _db[netPath];
-		
-		if (!item._isAsset){	
+
+		if (!item._isAsset){
 			var template = null;
-			
+
 			if (typeof templates[netPath] === 'string'){
 				//Specific template
 				template = templates[netPath];
@@ -30,13 +30,13 @@ module.exports = function matchTemplates(_db, templateFiles){
 				while(template === null){
 					var i = netPath.lastIndexOf(path.separator) + 1;
 					netPath = netPath.slice(0,i) + '_default';
-					
+
 					if (templates[netPath]){
 						template = templates[netPath];
 						break;
 					}
 					else if (i === 0){
-						throw new Error('Missing template _default.');
+						throw new Error('Missing template _default');
 						template = false;
 						break;
 					}
@@ -46,10 +46,10 @@ module.exports = function matchTemplates(_db, templateFiles){
 					}
 				}
 			}
-			
-			item._templateMatch = template.content;			
+
+			item._templateMatch = template.content;
 		}
 	}
-	
+
 	return _db;
 };
