070fc31d7ccfe98bb1dcd4737d2f0aa515356096
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-04-14 00:22:50 +0200

    Whitespace changes

--- build_templates.js
+++ build_templates.js
@@ -6,18 +6,18 @@ var objRef = require('objref');
 
 module.exports = function build_templates(files){
 	var _templates = {};
-	
+
 	files.map(function(template){
 		var treePath = path.tree(template.netPath);
 		var name = template.netPath.slice(treePath);
-		
+
 		var _templatesTree = templates;
 		if (treePath.length){
 			_templatesTree = objRef(_templates, treePath, path.delimiter, true);
 		}
-		
+
 		_templatesTree[name] = template.content;
 	});
-	
+
 	return _templates;
 };
