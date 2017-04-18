b65c1a42ba6e18d3e7a49cf1fd1783577a57f8ab
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-04-18 19:45:04 +0200

    New way to merge data

--- db.js
+++ db.js
@@ -4,28 +4,38 @@ var val = require('tidyval');
 var path = require('tidypath');
 var objRef = require('objref');
 
-module.exports = function build_db(files){
-	val(files).validate('array');
-	
-	var _db = {}
-	files.map(function(file){
-		val(file).validate('object');
-		val(file.netPath).validate('string');
-		
-		if (typeof _db[file.netPath] === 'undefined'){
-			_db[file.netPath] = {};
+module.exports = function _db(files){
+	//Deal with shared filenames
+	var data = {};
+	files.map(function (file){
+		var id = file.path;
+		if (typeof data[id] === 'undefined'){
+			data[id] = file.parsed;
 		}
-		
-		//Merge
-		for (var prop in file.parsed){
-			if (typeof _db[file.netPath][prop] === 'undefined'){
-				_db[file.netPath][prop] = file.parsed[prop];
+		else if (file.parsed._isMeta &&
+			file.parsed._ext === data[id]._ext){
+			//Try to merge (shallow merge)
+			for (var prop in file.parsed){
+				if (typeof data[id][prop] === 'undefined'){
+					data[id][prop] = file.parsed[prop];
+				}
+				else{
+					throw new Error([
+						'Property namespace conflict.',
+						'Meta data can not share property names.',
+					].join('\r\n'),);
+				}
 			}
-			else{
-				throw new Error('Filename/filecontent conflict');
-			}
+		}
+		else{
+			throw new Error([
+				'Filename conflict.',
+				'Only meta data files with same extension',
+				'may have the same filename.'
+			].join('\r\n'),);
+			//Namescpace conflig
 		}
 	});
 
-	return _db;
+	//
 };
