f7e5b6686082db97c1e328ae3423f37f6a9eb686
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-04-18 20:01:35 +0200

    Avoid shared filenames

--- db.js
+++ db.js
@@ -8,34 +8,17 @@ module.exports = function _db(files){
 	//Deal with shared filenames
 	var data = {};
 	files.map(function (file){
-		var id = file.path;
+		var id = path.rmExt(file.path);
 		if (typeof data[id] === 'undefined'){
 			data[id] = file.parsed;
 		}
-		else if (file.parsed._isMeta &&
-			file.parsed._ext === data[id]._ext){
-			//Try to merge (shallow merge)
-			for (var prop in file.parsed){
-				if (typeof data[id][prop] === 'undefined'){
-					data[id][prop] = file.parsed[prop];
-				}
-				else{
-					throw new Error([
-						'Property namespace conflict.',
-						'Meta data can not share property names.',
-					].join('\r\n'),);
-				}
-			}
-		}
 		else{
 			throw new Error([
-				'Filename conflict.',
-				'Only meta data files with same extension',
-				'may have the same filename.'
-			].join('\r\n'),);
-			//Namescpace conflig
+							'Filename conflict at ',
+							file.path
+							].join('\r\n'));
 		}
 	});
 
-	//
+	//Remove
 };
